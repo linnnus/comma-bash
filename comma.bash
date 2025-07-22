1c5d8d19abc72c044e99b118d5e3f8c7b8806fa1
Linnnus <linnnus@users.noreply.github.com>
2025-07-22 11:47:05 +0200

    Filter out wrapped packages

--- comma.bash
+++ comma.bash
@@ -2,11 +2,11 @@
 
 # Special case: if the program is `sudo` we actually run the utility with
 # `sudo` instead of looking for a package with `/bin/sudo`.
-sudo=""
-if [ "$1" = "sudo" ]; then
+specials=()
+while [ "$1" = "sudo" -o "$1" = "rlwrap" ]; do
+	specials+=("$1")
 	shift
-	sudo="sudo"
-fi
+done
 
 if [ $# -lt 1 ]; then
 	echo >&2 "Usage: $0 [sudo] program [args...]"
@@ -15,9 +15,10 @@ fi
 
 program="$1"
 
-# Find all packages that contain the binary we're trying to run, discarding
-# "meta" packages that just wrap other programs (currently just cope).
-mapfile -t derivations < <(@nix-index@/bin/nix-locate --top-level --minimal --whole-name /bin/"$program" | @toybox@/bin/grep -v '^cope.out$')
+# Find all packages that contain the binary we're trying to run, discarding:
+# - packages that just wrap other programs (currently just cope)
+# - "Unwrapped" packages (which are more useful in their wrapped version)
+mapfile -t derivations < <(@nix-index@/bin/nix-locate --top-level --minimal --whole-name /bin/"$program" | @toybox@/bin/egrep -v '^cope\.out|.*-unwrapped\.out$')
 
 case ${#derivations[@]} in
 	0)
@@ -39,4 +40,4 @@ if [ -z "$derivation" ]; then
 fi
 
 # TODO: The official comma uses -f <nixpkgs> if it is defined in path. We should do that too, depending on the behavior of nix-index.
-@nix@/bin/nix --extra-experimental-features 'nix-command flakes' shell nixpkgs#"$derivation" --command $sudo "$@"
+@nix@/bin/nix --extra-experimental-features 'nix-command flakes' shell nixpkgs#"$derivation" --command "${specials[@]}" "$@"
